92c9c1fcc3877e0c23c4948f21e8656b85c96800
FEMI The Data Scientist <33644535+sofarikasid@users.noreply.github.com>
2023-08-12 02:56:54 +0000

    search_and_rank

--- mylib/search.py
+++ mylib/search.py
@@ -3,37 +3,45 @@
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import CTransformers
-#from langchain.chains import RetrievalQA
-#from langchain import PromptTemplate
+
+# from langchain.chains import RetrievalQA
+# from langchain import PromptTemplate
 
 DB_FAISS_PATH = "mylib/vector_db"
 
 # Initialize embeddings and database outside of functions
-embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={'device': 'cpu'})
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2", model_kwargs={"device": "cpu"}
+)
 db = FAISS.load_local(DB_FAISS_PATH, embeddings)
 
 # Initialize the LLM model once
 llm = CTransformers(
     model="TheBloke/Llama-2-7B-Chat-GGML",
-    #model= "meta-llama/Llama-2-7b-chat-hf".
+    # model= "meta-llama/Llama-2-7b-chat-hf".
     model_type="llama",
     max_new_tokens=512,
-    temperature=0.5
+    temperature=0.5,
 )
 
 # Initialize the QA chain once
-#qa = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=db.as_retriever(search_kwargs={'k': 10}), return_source_documents=True)
-retriever = db.as_retriever(search_kwargs={"k": 10})
+# qa = RetrievalQA.from_chain_type(llm=llm, chain_type='stuff', retriever=db.as_retriever(search_kwargs={'k': 10}), return_source_documents=True)
+#retriever = db.as_retriever(search_kwargs={"k": 10})
+
 
 def final_result(query):
-    response = retriever.get_relevant_documents(query)
-    return response
+    docs_and_scores = db.similarity_search_with_score(query)
+    #response = retriever.get_relevant_documents(query)
+    return docs_and_scores
+
 
 if __name__ == "__main__":
     while True:
-        user_query = input("Please enter Retailer, Brand, or Category (type 'exit' to quit): ")
-        if user_query == 'exit':
+        user_query = input(
+            "Please enter Retailer, Brand, or Category (type 'exit' to quit): "
+        )
+        if user_query == "exit":
             break
         llm_response = final_result(user_query)
-        
+
         print(llm_response)
--- test_search.py
+++ test_search.py
@@ -2,24 +2,32 @@ import pytest
 from unittest.mock import patch
 from mylib.search import final_result
 
-@pytest.mark.parametrize("query, expected_response", [
-    ("walmart", [
-        "OFFER: Arber, at Walmart\ncombined: ARBER WALMART",
-        "OFFER: Back to the Roots Seeds, at Walmart\ncombined: Packaged Meals & Sides BACK TO THE ROOTS WALMART",
-        "OFFER: OxiClean™ Laundry Stain Removers, select varieties at Walmart\ncombined: WALMART OXICLEAN",
-        "OFFER: Gorton's at select retailers\ncombined: Frozen Seafood Jerky & Dried Meat Frozen Meals WALMART GORTONS",
-        "OFFER: Gorton's Air Fried Butterfly Shrimp, at Walmart\ncombined: Frozen Seafood Jerky & Dried Meat Frozen Meals WALMART GORTONS",
-        "OFFER: L’Oréal Paris Men Expert hair color, spend $9 at Walmart\ncombined: Hair Care LOREAL PARIS HAIR COLOR WALMART",
-        "OFFER: L'Oréal Paris Men Expert hair color, spend $19 at Walmart\ncombined: Hair Care LOREAL PARIS HAIR COLOR WALMART",
-        "OFFER: Back to the Roots Grow Kits at Walmart or The Home Depot\ncombined: Packaged Meals & Sides BACK TO THE ROOTS WALMART",
-        "OFFER: Cooked Perfect® Meatballs, at Walmart\ncombined: Frozen Chicken COOKED PERFECT WALMART",
-        "OFFER: Purex® laundry detergent, select varieties, at Walmart\ncombined: PUREX Laundry Supplies WALMART"
-    ]),
-])
+
+@pytest.mark.parametrize(
+    "query, expected_response",
+    [
+        (
+            "walmart",
+            [
+                "OFFER: Arber, at Walmart\ncombined: ARBER WALMART",
+                "OFFER: Back to the Roots Seeds, at Walmart\ncombined: Packaged Meals & Sides BACK TO THE ROOTS WALMART",
+                "OFFER: OxiClean™ Laundry Stain Removers, select varieties at Walmart\ncombined: WALMART OXICLEAN",
+                "OFFER: Gorton's at select retailers\ncombined: Frozen Seafood Jerky & Dried Meat Frozen Meals WALMART GORTONS",
+                "OFFER: Gorton's Air Fried Butterfly Shrimp, at Walmart\ncombined: Frozen Seafood Jerky & Dried Meat Frozen Meals WALMART GORTONS",
+                "OFFER: L’Oréal Paris Men Expert hair color, spend $9 at Walmart\ncombined: Hair Care LOREAL PARIS HAIR COLOR WALMART",
+                "OFFER: L'Oréal Paris Men Expert hair color, spend $19 at Walmart\ncombined: Hair Care LOREAL PARIS HAIR COLOR WALMART",
+                "OFFER: Back to the Roots Grow Kits at Walmart or The Home Depot\ncombined: Packaged Meals & Sides BACK TO THE ROOTS WALMART",
+                "OFFER: Cooked Perfect® Meatballs, at Walmart\ncombined: Frozen Chicken COOKED PERFECT WALMART",
+                "OFFER: Purex® laundry detergent, select varieties, at Walmart\ncombined: PUREX Laundry Supplies WALMART",
+            ],
+        ),
+    ],
+)
 @patch("builtins.input", side_effect=["query_1", "query_2"])
 def test_final_result(mock_input, query, expected_response):
     response = final_result(query)
     assert type(response) == type(expected_response)
+
 
 if __name__ == "__main__":
     pytest.main()
